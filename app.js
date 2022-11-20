747ed73ef04f1f2e65f59f52f2afc53c51cdbeb9
Marcelogonzalez7 <ing.mjgq7@gmail.com>
2022-11-20 16:41:38 -0300

    add new ecommerce landing page

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var app = new Vue({
-    el: '#center',
+    el: '#app',
     data: {
         message: 'Hola Orión',
-        contador: 0
+        contador: 0,
+        bienvenida: "Bienvenido a Vue 2",
+        carta: {
+            imagen: "img/hero4.png",
+            home_image: "img/home.png",
+            titulo: "Sticker 1",
+            descripcion: "Hermosos sticker que se pegan a la pared",
+            boton: "Ver",
+            precio: 2990.00
+        },
+        precioIva:0,
+        nombre: "",
+        numero:290
     },
     methods: {
         sumar() {
@@ -12,6 +24,19 @@ var app = new Vue({
         restar() {
             app.contador = app.contador - 1
             document.getElementById('counting').innerHTML = app.contador;
+        },
+        mostrarPrecio(){
+            alert(`Este hermoso ${this.carta.titulo} sale ${this.carta.precio}`)
+        },
+        calcularIva(){
+            this.precioIva = this.carta.precio * 1.21
+            return this.precioIva; 
+        }
+    },
+    computed:{
+        mostrarNombre(){
+            // la obligación de return algo
+            return this.nombre.toUpperCase()
         }
     }
 })
