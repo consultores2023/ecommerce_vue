842a02f48bb5109fd9904d130df576b83fc8420c
Marcelogonzalez7 <ing.mjgq7@gmail.com>
2022-11-20 22:48:02 -0300

    fix

--- app.js
+++ app.js
@@ -14,8 +14,31 @@ var app = new Vue({
         },
         precioIva:0,
         nombre: "",
-        numero:290
+        numero: 290,
+        navbarItems: [
+            { label: "Shop", href: "shop.html" },
+            { label: "About", href: "about.html" },
+            { label: "Blog", href: "blog.html" },
+            { label: "Contact", href: "contact.html" },
+            { label: "Cart", href: "cart.html" }],
+        heroItems: ["Trade-in-offer", "Super Value deals", "On all product", "Save more with coupons", "Shop Now"],
+        featureItems: [{ label: "Envio gratutito", href: "img/features/f1.png" },
+                        { label: "Orden Online", href: "img/features/f2.png" },
+                        { label: "Ahorra Dinero", href: "img/features/f3.png" },
+                        { label: "Promotions", href: "img/features/f4.png" },
+            { label: "Happy Sell", href: "img/features/f5.png" },
+            { label: "24/7 Support", href: "img/features/f6.png" }],
+        productItems: [{ brand: "adidas", name: "Cartoon Astronaut T-shirt", price: "$78", href: "img/products/f1.jpg" },
+            { brand: "adidas", name: "Cartoon Astronaut T-shirt", price: "$78", href: "img/products/f2.jpg" },
+            { brand: "adidas", name: "Cartoon Astronaut T-shirt", price: "$78", href: "img/products/f3.jpg" },
+            { brand: "adidas", name: "Cartoon Astronaut T-shirt", price: "$78", href: "img/products/f4.jpg" },
+            { brand: "adidas", name: "Cartoon Astronaut T-shirt", price: "$78", href: "img/products/f5.jpg" },
+            { brand: "adidas", name: "Cartoon Astronaut T-shirt", price: "$78", href: "img/products/f6.jpg" },
+            { brand: "adidas", name: "Cartoon Astronaut T-shirt", price: "$78", href: "img/products/f7.jpg" },
+            {brand: "adidas", name: "Cartoon Astronaut T-shirt", price:"$78", href: "img/products/f8.jpg"}]
+    
     },
+    
     methods: {
         sumar() {
             app.contador = app.contador + 1
@@ -31,6 +54,9 @@ var app = new Vue({
         calcularIva(){
             this.precioIva = this.carta.precio * 1.21
             return this.precioIva; 
+        },
+        returnValue() {
+            return this.contador
         }
     },
     computed:{
